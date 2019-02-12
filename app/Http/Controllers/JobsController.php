485f276ff706ce12ded464c1c395c36d2951f757
IT!alo7895123 <italolt10@gmail.com>
2019-02-12 10:05:46 -0200

    create crud

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -2,9 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Job;
 use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class JobsController extends Controller
 {
-    //
+    public function index()
+    {
+        $jobs = Job::with('company')->get();
+        return response()->json($jobs);
+    }
+
+    public function show($id)
+    {
+        $job = Job::with('company')->find($id);
+
+        if(!$job) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        return response()->json($job);
+    }
+
+    public function store(Request $request)
+    {
+        $job = new Job();
+        $job->fill($request->all());
+        $job->save();
+
+        return response()->json($job, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $job = Job::find($id);
+
+        if(!$job) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        $job->fill($request->all());
+        $job->save();
+
+        return response()->json($job);
+    }
+
+    public function destroy($id)
+    {
+        $job = Job::find($id);
+
+        if(!$job) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        $job->delete();
+    }
 }
